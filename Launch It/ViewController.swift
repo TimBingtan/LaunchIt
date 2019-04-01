0177d311b197667a2cead42f74549d9439382e45
Tim <tbingtan@uci.edu>
2019-04-01 14:23:21 -0700

    Initial Commit

---
+++ Launch It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Launch It
+//
+//  Created by Tim Bingtan on 4/1/19.
+//  Copyright © 2019 Tim Bingtan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
