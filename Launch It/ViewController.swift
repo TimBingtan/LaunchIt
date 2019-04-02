9c7880318c440d16bd33cc84cb237896b627fdc0
Tim <tbingtan@uci.edu>
2019-04-02 01:11:01 -0700

    Rocket launch progress.

--- Launch It/ViewController.swift
+++ Launch It/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var ONLabel: UILabel!
+    
+    var player: AVAudioPlayer!  // Implicitly unwrapped optional
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
-
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerButton.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame  = CGRect(x: 0, y: 140, width: 375, height: 402)
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.ONLabel.isHidden = false
+        }
+    }
+    
 }
 
